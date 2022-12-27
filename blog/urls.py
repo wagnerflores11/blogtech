f2e9df9595d108052b7c9164df6193ea63fb5f3a
Wagner Flores <wagner.flores@somosquiq.com>
2022-12-27 17:51:51 -0300

    feat: route-create

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('news/', views.news_list, name='news_list'),
+    path('create/', views.create_news, name='create_news'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import News
+
+from django.contrib.auth.decorators import login_required
+from .forms import NewsForm
 
 def news_list(request):
     news = News.objects.filter(published=True)
     return render(request, 'blog/news_list.html', {'news': news})
+
+
+
+
+@login_required
+def create_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = form.save()
+            news.author = request.user
+            news.save()
+            return redirect('news_list')
+    else:
+        form = NewsForm()
+    return render(request, 'blog/create_news.html', {'form': form})
